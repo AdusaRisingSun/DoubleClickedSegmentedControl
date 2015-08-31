3d019e5381e095448d9d994afd1ce4fa69453c23
AdusaRisingSun <Adusa@AdusadeMac-mini.local>
2015-08-31 15:56:01 +0800

    Initial Commit

---
+++ DoubleClickedSegmentedControl/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DoubleClickedSegmentedControl
+//
+//  Created by Adusa on 15/8/31.
+//  Copyright (c) 2015年 Adusa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
